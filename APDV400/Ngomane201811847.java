587514117e1a37bb015f89e51e8dfc598840a5ab
Mjongoza <107530251+Mjongoza@users.noreply.github.com>
2022-06-15 10:59:19 +0200

    Add files via upload

---
+++ APDV400/Ngomane201811847.java
@@ -0,0 +1,11 @@
+
+public class Ngomane201811847{
+		public static void main(String [] args ) {
+
+		int a=5;
+		int b=10;
+		int c= a*b;
+
+	System.out.println( "a=5 * b=10 is equal to" +c );
+	}
+}
